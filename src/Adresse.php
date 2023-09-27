bf77a21e3d8becb82f46aa69d0c479459074679e
Chris Tenday <ctenday@gmail.com>
2023-09-27 12:51:31 +0100

    update

--- src/Adresse.php
+++ src/Adresse.php
@@ -8,8 +8,60 @@ class Adresse
 {
     private Dbconnect $dbconnect;
 
+    private $province;
+    private $commune;
+    private $quartier;
+    private $avenue;
+    private $numero;
+
     public function __construct($province,$commune,$quartier,$avenue,$numero)
     {
-        new Dbconfig()
+        $dbConfig=new Dbconfig(dbHostname: "localhost",dbUsername: "root",dbUserPassword: "",dbName: "milleniumpayroll");
+
+        $this->dbconnect=new Dbconnect($dbConfig);
+
+        $this->province=$province;
+        $this->commune=$commune;
+        $this->quartier=$quartier;
+        $this->avenue=$avenue;
+        $this->numero=$numero;
+    }
+
+    /**
+     * Enregistrer une adresse dans la db.
+     * @return $id: l'id de l'adresse.
+     */
+    public function save()
+    {
+        $this->dbconnect->setTable("adresses");
+
+        $data['province']=$this->province;
+        $data['commune']=$this->commune;
+        $data['quartier']=$this->quartier;
+        $data['avenue']=$this->avenue;
+        $data['numero']=$this->numero;
+
+        $id=$this->dbconnect->insert($data);
+
+        return $id;
+    }
+
+    /**
+     * Recuperer une adresse de la db par son ID.
+     * @param $adressId
+     * @return mixed|null
+     */
+    public function get($adressId)
+    {
+        $this->dbconnect->where("adresse_id","=",$adressId);
+
+        $ad=$this->dbconnect->select();
+        if(!$ad)
+        {
+            return null;
+        }
+        $ad=$ad[0];
+
+        return $ad;
     }
 }
